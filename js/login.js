fe9b940039c623ba9602c785c68a07f8e1b94a7d
micsenande <aym1899@gmail.com>
2020-09-20 21:57:49 -0300

    Entrega obligatoria
    Entrega obligatoria 20)9/2020

--- js/login.js
+++ js/login.js
@@ -1,22 +1,16 @@
-// CUANDO TERMINE DE CARGARSE EL DOM
 document.addEventListener("DOMContentLoaded", function(e){
+     document.getElementById("buttonIngresoLogin").addEventListener("click", function (e) {
 
-    // SE CREAR UN EVENTO, QUE ESCUCHA AL BOTÓN DE INGRESAR 
-    document.getElementById("buttonIngresoLogin").addEventListener("click", function (e) {
-
-    // EXTRAEMOS LOS DATOS
     let inputEmail = document.getElementById("inputEmailLogin");
     let inputPassword = document.getElementById("inputPasswordLogin");
         
-    // VERIFICAMO QUE LOS CAMPOS ESTÉN COMPLETOS
     let camposCompletos = true;
     if (inputEmail.value === '' || inputPassword.value === ''){
         camposCompletos = false;
         alert("Debes ingresar tus datos para continuar.");
     }
 
-        if (camposCompletos) { // SI LOS DATOS ESTÁN COMPLETOS
-        // GUARDAMOS EN EL STORAGE LA INFORMACION INGRESADA
+        if (camposCompletos) { 
         localStorage.setItem('user-logged', JSON.stringify({ email: inputEmail.value }));
         window.location = 'home.html';
     }
--- js/product-info.js
+++ js/product-info.js
@@ -1,127 +1,132 @@
 var product = {};
-var productComments = [];
+var arrayComments = [];
+var productsArray = [];
 
-// ESTA FUNCION RECIBE UN OBJETO PRODUCTO, Y LOS MUESTRA EN PANTALLA
-function mostrarProductoConSusImagenes(producto) {
 
-    let imagenesDelProducto = "";
+function showRelatedProducts(arrayListado, arrayRelacionados) {
+    let contenido = "";
+    let product = arrayListado;
 
-    imagenesDelProducto = `
-    <img class="img" src= "${producto.images[0]}" width="300px" height = "200px">
-    <img class="img" src= "${producto.images[1]}" width="300px" height = "200px">
-    <img class="img" src= "${producto.images[2]}" width="300px" height = "200px">
-    <img class="img" src= "${producto.images[3]}" width="300px" height = "200px">
-    <img class="img" src= "${producto.images[4]}" width="300px" height = "200px">
+    arrayRelacionados.forEach(function (i) {
+        contenido += `
+        <a href="product-info.html" class=" w-100 d-flex list-group-item justify-content-center list-group-item-action">
+                    <div>
+                        <img src="${product[i].imgSrc}" class="img-thumbnail">  
+                    </div>  
+                    <div>
+                    <h4 class="mb-1 text-center">${product[i].name}</h4>
+                        <p class="text-center">${product[i].cost}${product[i].currency}</p>
+                    </div>
+                </a>
+        `
+        /*
+        contenido += "Nombre: " + product[i].name + "<br>";
+        contenido += "Precio: " + product[i].cost + "<br>";
+       // contenido += "Descripción: " + product[i].description + "<br>";
+       contenido += `
+        <img class="img-thumbnail" src= "${product[i].imgSrc}" width="60px" height = "40px">`;
+        contenido += `<a href="product-info.html"><button style ="float: right;" class= "btn btn-light active";>Ver más</button></a><br>`
+        contenido += `<br><hr><br>`
+        */
+    });
+    document.getElementById("relatedProduct").innerHTML = contenido;
+}
+
+function showProduct(product, arrayComments) {
+    let imgs = "";
+    let comments = "<hr>";
+
+    imgs += `
+    <img class="img" src= "${product.images[0]}" width="300px" height = "200px">
+    <img class="img" src= "${product.images[1]}" width="300px" height = "200px">
+    <img class="img" src= "${product.images[2]}" width="300px" height = "200px">
+    <img class="img" src= "${product.images[3]}" width="300px" height = "200px">
+    <img class="img" src= "${product.images[4]}" width="300px" height = "200px">
     `;
 
-    document.getElementById("categoryName").innerHTML = producto.name;
-    document.getElementById("categoryDescription").innerHTML = producto.description;
-    document.getElementById("productCount").innerHTML = producto.cost;
-    document.getElementById("soldCount").innerHTML = producto.soldCount;
-    document.getElementById ("category").innerHTML = producto.category;
+    arrayComments.forEach(function (comment) {
+        let puntos = "";
+        comments += `
+        <strong>${comment.user} </strong> dice: <br>
+        <p>${comment.description}</p>`;
 
-    document.getElementById("productImagesGallery").innerHTML = imagenesDelProducto;
-}
-
-// ESTA FUNCION RECIBE UNA ARRAY DE COMENTARIOS, Y MUESTRA EN PANTALLA LOS COMENTARIOS
-function mostrarComentariosDelProducto(comentarios) {
-
-    let comentariosParaMostrar = "<hr>";
-
-    comentarios.forEach(function (itemComentario) {
-        
-        let puntosDelComentario = "";
-
-        comentariosParaMostrar += `
-            <strong>${itemComentario.user} </strong> dice: <br>
-            <p>${itemComentario.description}</p>
-        `;
-
-        for (let i = 1; i <= itemComentario.score; i++) {
-            puntosDelComentario += `<span class= "fa fa-star checked"></span>`;
+        for (let i = 1; i <= comment.score; i++) {
+            puntos += `<span class= "fa fa-star checked"></span>`;
         }
-        for (let i = itemComentario.score + 1; i <= 5; i++) {
-            puntosDelComentario += `<span class= "fa fa-star"></span>`;
+        for (let i = comment.score + 1; i <= 5; i++) {
+            puntos += `<span class= "fa fa-star"></span>`;
         }
 
-        comentariosParaMostrar += `<sub>${itemComentario.dateTime}</sub><br>`;
-        comentariosParaMostrar += `<div style= "text-align: right;">${puntosDelComentario}</div><br><hr>`;
+        comments += `<sub>${comment.dateTime}</sub><br>`;
+        comments += `<div style= "text-align: right;">${puntos}</div><br><hr>`;
+
+
 
     });
-
-    document.getElementById("comentarios").innerHTML = comentariosParaMostrar;
+    //document.getElementById("productImagesGallery").innerHTML = imgs;
+    document.getElementById("comentarios").innerHTML = comments;
 }
 
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            arrayComments = resultObj.data;
+        }
+    })
+})
 
-document.addEventListener("DOMContentLoaded", function () {
-    
-    // EXTRAEMOS LA INFORMACION DEL PRODUCTO
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             product = resultObj.data;
+
+            let productNameHTML = document.getElementById("categoryName");
+            let productDescriptionHTML = document.getElementById("categoryDescription");
+            let productCountHTML = document.getElementById("productCount");
+            let productsoldCount = document.getElementById("soldCount");
+            let productcategory = document.getElementById("category");
+
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCountHTML.innerHTML = product.cost;
+            productsoldCount.innerHTML = product.soldCount;
+            productcategory.innerHTML = product.category;
+
+
+            showProduct(product, arrayComments);
         }
     });
+    
+   getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data;
 
-    // EXTRAEMOS LOS COMENTARIOS DEL PRODUCTO
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            productComments = resultObj.data;
+            showRelatedProducts(productsArray, product.relatedProducts);
+
         }
+    })
+})
 
-        // MOSTRAMOS EL PRODUCTO
-        mostrarProductoConSusImagenes(product);
-        // MOSTRAMOS LOS COMENTARIOS DEL PRODUCTO
-        mostrarComentariosDelProducto(productComments)
+/*document.getElementById("enviarComentario").addEventListener("click", function(){
+    let now= new Date();
+    let dateTime = `${(now.getFullYear)()-${(now.getMonth()+1}}-${now.getDate()}`;
+    dateTime += `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
 
-    });
+    let nuevo = {
+        score: parseInt(document.getElementById("productCalification").value;
+        description: document.getElementById("comentar").value;
+        user: JSON.parse(localStorage.getItem("User-Logged")).email;
+        dateTime: dateTime
 
-});
+        };
 
-// CUANDO EL USUARIO AGREGUE UN NUEVO COMENTARIO
-document.getElementById("enviarComentario").addEventListener("click", function () {
+        arrayComments.push(nuevo);
 
-    // EXTRAEMOS EL COMENTARIO
-    let elementoTextArea = document.getElementById("productDescription");
-    let comentrario = elementoTextArea.value;
-    let estrellas = parseInt(document.getElementById("estrellas").value);
+        showProduct(product,arrayComments)
 
-    comentarioParaAgregar = {};
-
-    // EXTRAEMOS EL NOMBRE DE USUARIO
-    let email = ""; 
-    let userLogged = localStorage.getItem("user-logged");
-    if (userLogged) {
-        userLogged = JSON.parse(userLogged);
-        email = userLogged.email;
-    } else {
-        email = "Anonimo";
     }
 
-    // EXTRAEMOS LA FECHA Y HORA
-    let dateTime = new Date();
-    let fechaHora = `
-    ${dateTime.getFullYear()}-
-    ${dateTime.getMonth() + 1}-
-    ${dateTime.getDate()} ${dateTime.getHours()}:
-    ${dateTime.getMinutes()}:
-    ${dateTime.getSeconds()}
-    `;
-
-    // CREAMOS EL COMENTARIO
-    comentarioParaAgregar = {
-        score: estrellas,
-        description: comentrario,
-        user: email,
-        dateTime: fechaHora,
-    };
-
-    // AGREGAMOS EL COMENTARIO A LOS OTROS COMENTARIOS
-    productComments.push(comentarioParaAgregar);
-
-    // MOSTRAMOS NUEVAMENTE LOS COMENTARIOS
-    mostrarComentariosDelProducto(productComments);
-
-});
-
-
-
+    }
+});*/
